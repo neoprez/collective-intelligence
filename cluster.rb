3b7e724f825d408e85fe427b7777e0937c443ff4
Rodny Perez <rodnyeperez@gmail.com>
2015-10-31 22:48:01 -0400

    Add pearson score for clusters

--- cluster.rb
+++ cluster.rb
@@ -23,4 +23,24 @@ module Cluster
 
 		return row_names, col_names, data
 	end
+
+	def self.pearson(v1,v2)
+		# simple sums
+		sum1 = v1.inject(:+)
+		sum2 = v2.inject(:+)
+
+		# sum of the squares
+		sum1Sq = v1.map{ |v| v**2 }.inject(:+)
+		sum2Sq = v2.map{ |v| v**2 }.inject(:+)
+
+		# Sum of the products
+		pSum = (0...v2.length).to_a.map{ |i| v1[i] * v2[i] }.inject(:+)
+
+		# Calculate r (Pearson score)
+		num = pSum-(sum1*sum2/v1.length.to_f)
+		den = Math.sqrt((sum1Sq-(sum1**2)/v1.length.to_f)*(sum2Sq-(sum2**2)/v1.length.to_f))
+		return 0 if den == 0
+	
+		return 1.0-num/den
+	end
 end
